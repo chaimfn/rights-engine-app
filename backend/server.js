b2a77c761f45f99cc932d6aa45ed7c4f9f5cdf98
chaimfn <chaim.fn@gmail.com>
2024-06-30 13:36:39 +0300

    backend docker

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,32 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
-const MODE = process.env.MODE || "production"
+const MODE = process.env.MODE || "prod"
+const DB_CONN_STR = process.env.DB_CONN_STR;
 
+const { MongoClient } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-const dbo = require("./dbo")
-
 const app = express();
 
+async function createMongoClient() {
+	let mongoClient;
+
+	try {
+		mongoClient = new MongoClient(DB_CONN_STR);	
+		await mongoClient.connect();
+		console.log('Connected to MongoDB');
+		return mongoClient;
+	}
+	catch (err) {
+		console.error('Failed to connect to MongoDB', err);
+		process.exit();
+	}
+}
+
+
 async function getFromDB() {
-	let mongoClient = await dbo.mongoClient();
+	let mongoClient = await createMongoClient();
 	let db = mongoClient.db("rights-engine");
 	let collection = db.collection("rights");
 	let rights = await collection.find().toArray();
@@ -35,6 +51,13 @@ app.get("/rights", async (req, res) => {
 	console.log(resBody);
 	res.status(200).send(resBody);
 })
+app.get("/hz", (req, res) => {
+	console.log("hz");
+	res.status(200).send("hz");
+})
+app.get("/", (req, res) => {
+	res.status(200).send("ok");
+});
 app.listen(PORT, () => {
 	console.log(`Server is running on port: ${PORT}`);
 });
