ba00e209237af5bc85042c19cb950be550b08c1b
chaimfn <chaim.fn@gmail.com>
2024-07-01 11:46:34 +0300

    console

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,8 @@ function App(props) {
   const [uncertainRights, setUncertainRights] = useState(null);
   const [fields, setFields] = useState(null);
   const [txtarea, setTextarea] = useState(null);
+  const [message, setMessage] = useState(null);
+  const [msgType, setMessageType] = useState("info");
 
   function setExamplePerson(e, person) {
     setTextarea(JSON.stringify(person, null, 2));
@@ -22,11 +24,22 @@ function App(props) {
       _person = JSON.parse(txtarea);
     }
     catch (err) {
-      return console.error("failed to parse txtaria to Person", err)
+      let msg = {
+        title: "Failed to parse txtarea to Person",
+        msg: err.message
+      };
+      setMessage(JSON.stringify(msg, null, 2));
+      setMessageType("err");
+      return console.error(msg, err);
     }
 
     if (_person == null || _person == undefined) {
-      return console.error("person is null/undefined");
+      let msg = {
+        title: "Person is null/undefined",
+      };
+      setMessage(JSON.stringify(msg, null, 2));
+      setMessageType("err");
+      return console.error(msg);
     }
 
     let entitled = [], notEntitled = [], uncertain = [];
@@ -43,32 +56,35 @@ function App(props) {
       });
     }
     catch (err) {
-      console.error(err)
+      let msg = {
+        title: "Error in right.isMatched()",
+        msg: err.message
+      };
+      setMessage(JSON.stringify(msg, null, 2));
+      setMessageType("err");
+      return console.error(err)
     }
     let t2 = new Date();
 
     setEntitledRights(entitled);
     setNotEntitledRights(notEntitled);
     setUncertainRights(uncertain);
-    let log = {
-      entitled: entitled?.length,
-      notEntitled: notEntitled?.length,
-      uncertain: uncertain.length,
-      sortTime: t2.getTime() - t1.getTime()
-    };
-    console.log(log);
-    // alert(JSON.stringify(log, null, 2))
     let exceptFields = Object.keys(_person)?.filter(key => _person[key] != null);
     let t3 = new Date();
     let _fields = RightModel.getPopularFields(uncertain, Condition.And, exceptFields);
     let t4 = new Date();
     setFields(_fields)
-    log = {
+    let log = {
+      entitled: entitled?.length,
+      notEntitled: notEntitled?.length,
+      uncertain: uncertain.length,
+      sortTime: t2.getTime() - t1.getTime(),
       popularFields: _fields.length,
       time: t4.getTime() - t3.getTime()
     }
     console.log(log)
-    // alert(JSON.stringify(log, null, 2))
+    setMessage(JSON.stringify(log, null, 2))
+    setMessageType("info");
   }
 
   function onTxtareaChange(e) {
@@ -85,28 +101,33 @@ function App(props) {
       .then(data => {
         _rights = data?.rights?.map(item => RightModel.convert(item));
         let t3 = new Date();
-        let log = {
-          rights: _rights?.length,
-          backendTime: data?.serverTime,
-          frontTime: t2.getTime() - t1.getTime(),
-          convertTime: t3.getTime() - t2.getTime()
-        };
-        console.log(log);
-        // alert(JSON.stringify(log, null, 2))
         setAllRights(_rights);
         setUncertainRights(_rights);
         let t4 = new Date();
         let _fields = RightModel.getPopularFields(_rights);
         let t5 = new Date();
         setFields(_fields)
-        log = {
-          popularFields: _fields.length,
-          time: t5.getTime() - t4.getTime()
-        }
-        console.log(log)
-        // alert(JSON.stringify(log, null, 2))
+        let log = {
+          rights: _rights?.length,
+          backendTime: data?.serverTime,
+          frontTime: t2.getTime() - t1.getTime(),
+          convertTime: t3.getTime() - t2.getTime(),
+          sortedFields: _fields.length,
+          sortTime: t5.getTime() - t4.getTime()
+        };
+        console.log(log);
+        setMessage(JSON.stringify(log, null, 2))
+        setMessageType("info");
       })
-      .catch(err => console.error("Failed to get data", err))
+      .catch(err => {
+        let msg = {
+          title: "Failed to get data",
+          msg: err.message
+        };
+        setMessage(JSON.stringify(msg, null, 2));
+        setMessageType("err");
+        console.error(msg, err);
+      })
   }, []);
 
   return (
@@ -120,8 +141,12 @@ function App(props) {
         }
       </header>
       <main>
+        <div className='console'>
+          <strong className='center'>Console</strong>
+          <pre className={`msg ${msgType}`}>{message}</pre>
+        </div>
         <div className='center'>
-          <strong style={{marginRight: "10px"}}>פרטים אישיים</strong>
+          <strong style={{ marginRight: "10px" }}>פרטים אישיים</strong>
           <div className='person-examples'>
             <strong>דוגמאות:</strong>
             {persons?.map((person, i) => <button key={i} title={person?.description} onClick={(e) => setExamplePerson(e, person)}>{person?.name}</button>)}
