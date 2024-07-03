9bc001674833a7fdbe90e44422894484d963fbfe
chaimfn <chaim.fn@gmail.com>
2024-07-03 13:10:43 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Ready from "./Ready";
 
+
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route index element={<Home />} />
                 <Route path="ready" element={<Ready />} />
+                <Route path="hz" element={"hz"} />
             </Routes>
         </BrowserRouter>
     );
 }
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(<App />);
