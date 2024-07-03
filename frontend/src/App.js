67e6123e2be881c3e3a2c10a89409bd7db5fbabe
chaimfn <chaim.fn@gmail.com>
2024-07-03 14:27:21 +0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import Ready from "./Ready";
-
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route index element={<Home />} />
-                <Route path="ready" element={<Ready />} />
-                <Route path="hz" element={"hz"} />
-            </Routes>
-        </BrowserRouter>
-    );
+    return <BrowserRouter>
+        <Routes>
+            <Route index element={<Home />} />
+        </Routes>
+    </BrowserRouter>;
 }
