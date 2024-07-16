b960953f4f18f4eec82d41143550a5c7b206c784
chaimfn <chaim.fn@gmail.com>
2024-07-16 17:32:59 +0300

    .

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 require("dotenv").config();
+const fs = require("fs")
+
 
 const PORT = process.env.PORT || 5000;
 const MODE = process.env.MODE || "prod"
 const DB_CONN_STR = process.env.DB_CONN_STR;
+const DLMTR = "---------";
+const DATA_FILE = "./data/rights.json"
 
 const { MongoClient } = require("mongodb");
 const express = require("express");
@@ -15,11 +19,11 @@ async function createMongoClient() {
 	try {
 		mongoClient = new MongoClient(DB_CONN_STR);
 		await mongoClient.connect();
-		console.log('Connected to MongoDB');
+		console.log('connected to mongodb');
 		return mongoClient;
 	}
 	catch (err) {
-		console.error('Failed to connect to MongoDB', err);
+		console.error('failed to connect to mongodb', err);
 		process.exit();
 	}
 }
@@ -35,24 +39,32 @@ async function getFromDB() {
 }
 
 async function getFromFile() {
-	return require("./data/rights.json");
+	return require(DATA_FILE);
 }
 
 app
 	.use(cors())
 	.use(express.json())
 	.get("/hz", (req, res) => {
-		console.log("hz");
-		res.status(200).send("hz");
+		console.log("/hz");
+		console.log(DLMTR);
+		res.status(200).send("/hz");
 	})
 	.get("/ready", async (req, res) => {
-		console.log("ready");
+		let msg = "/ready"
+		console.log(msg);
 		let status = 200;
-		let msg = "ready";
 		try {
-			let mongoClient = await createMongoClient();
-			mongoClient.db("rights-engine");
-			mongoClient.close();
+			if (MODE == "dev") {
+				if (!fs.existsSync(DATA_FILE))
+					throw new Error(`'${DATA_FILE}' not exists`)
+			}
+			else {
+				let mongoClient = await createMongoClient();
+				mongoClient.db("rights-engine");
+				mongoClient.close();
+			}
+			console.log("ok")
 		}
 		catch (err) {
 			status = 503;
@@ -60,10 +72,12 @@ app
 			msg = "not ready";
 		}
 		finally {
+			console.log(DLMTR)
 			res.status(status).send(msg);
 		}
 	})
 	.get("/rights", async (req, res) => {
+		console.log("/rights")
 		let t1 = new Date();
 		let rights = MODE == "dev" ?
 			await getFromFile() :
@@ -73,12 +87,16 @@ app
 		let resBody = { rights: rights.length, mode: MODE, serverTime: diff }
 		console.log(resBody);
 		resBody.rights = rights;
+		console.log(DLMTR)
 		res.status(200).send(resBody);
 	})
 	.get("/", (req, res) => {
+		console.log("/")
+		console.log(DLMTR)
 		res.status(200).send("ok");
 	})
 	.listen(PORT, () => {
-		console.log(`Server is running on port: ${PORT}`);
+		console.log(`Server is running on port: ${PORT}. Mode: ${MODE}`);
+		console.log(DLMTR)
 	});
 
